25964c3a2b758f4845cf78cf35221539316c7daf
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-12 10:07:29 -0300

    Implementação do controller de criação de nova categoria.

--- src/Controllers/CategoryController.js
+++ src/Controllers/CategoryController.js
@@ -5,12 +5,26 @@ class CategoryController {
     try {
       const categories = await db.Category.findAll();
       return res.json(categories);
-    } catch {
-      return res.status(500).json({ message: "Internal error." });
+    } catch (error) {
+      const message = error.statusCode ? error.message : "Internal error.";
+      return res.status(error.statusCode || 500).json({ message });
     }
   }
 
-  static async create(req, res) {}
+  static async create(req, res) {
+    const data = req.body;
+
+    try {
+      const newCategory = await db.Category.create(data);
+
+      if (!newCategory) throw new ApiError("Internal error.", 500);
+
+      return res.status(201).json(newCategory);
+    } catch (error) {
+      const message = error.statusCode ? error.message : "Internal error.";
+      return res.status(error.statusCode || 500).json({ message });
+    }
+  }
 }
 
 module.exports = { CategoryController };
