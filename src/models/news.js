d92367de0e498c43cd4382822579ffb04d9f2210
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-11 21:39:56 -0300

    Definindo os relacionamentos nos models.

--- src/models/news.js
+++ src/models/news.js
@@ -2,13 +2,11 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class News extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Category, {
+        foreignKey: "category_id",
+        as: "category"
+      });
     }
   }
   News.init(
