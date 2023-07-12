9f9c776e9badfa703505843509a8f27dd3f2bdc1
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-11 22:53:58 -0300

    Implementação da classe ApiError para lidar com exceções.

---
+++ src/errors/ApiError.js
@@ -0,0 +1,11 @@
+class ApiError extends Error {
+  #statusCode;
+  constructor(message, statusCode) {
+    super(message);
+    this.#statusCode = statusCode;
+  }
+
+  get statusCode() {
+    return this.#statusCode;
+  }
+}
