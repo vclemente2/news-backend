00fd2c57d8c1db4bc2b4e6199b3cb4dd2c1dd15e
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-20 09:07:48 -0300

    Cors.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App {
   #app;
   constructor() {
     this.#app = express();
-    this.#app.use(cors());
+    this.#app.use(
+      cors({ origin: "https://cute-tan-buffalo-cuff.cyclic.app/" })
+    );
     this.#app.use(express.json());
     this.#app.use(new Routes().getRoute());
   }
