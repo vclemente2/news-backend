860645c96c1607dc82705befc0f7dd6eb51613a5
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-27 13:46:54 -0300

    Inclusão de imagens default no seeder de notícias.

--- src/connection/seeders/20230712004151-populate-news.js
+++ src/connection/seeders/20230712004151-populate-news.js
@@ -11,6 +11,8 @@ module.exports = {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
           author: "John Doe",
           category_id: getRandomCategoryId(),
+          image:
+            "https://news-page-production.s3.us-east-005.backblazeb2.com/1/stock-photo-starry-dark-sky-sprucesin-carpathian.jpeg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -20,6 +22,8 @@ module.exports = {
             "Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
           author: "Jane Smith",
           category_id: getRandomCategoryId(),
+          image:
+            "https://news-page-production.s3.us-east-005.backblazeb2.com/2/stock-photo-dark-sky-full-shiny-stars.jpeg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -29,6 +33,8 @@ module.exports = {
             "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
           author: "Mark Johnson",
           category_id: getRandomCategoryId(),
+          image:
+            "https://news-page-production.s3.us-east-005.backblazeb2.com/3/images.jpeg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -38,6 +44,8 @@ module.exports = {
             "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
           author: "Sarah Thompson",
           category_id: getRandomCategoryId(),
+          image:
+            "https://news-page-production.s3.us-east-005.backblazeb2.com/4/stock-photo-tranquil-sunset-seascape-cloudy-sky.jpeg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -56,6 +64,8 @@ module.exports = {
             "Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           author: "Emily Wilson",
           category_id: getRandomCategoryId(),
+          image:
+            "https://news-page-production.s3.us-east-005.backblazeb2.com/6/images+(1).jpeg",
           createdAt: new Date(),
           updatedAt: new Date()
         }
